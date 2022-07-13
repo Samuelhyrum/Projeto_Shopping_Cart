8b3527408aab6ba17d409d36b93447dec7690a82
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-13 16:01:20 -0300

    Req 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (ItemID) => {
+  try{
+    const url = `https://api.mercadolibre.com/items/${ItemID}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch(err){
+    return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
