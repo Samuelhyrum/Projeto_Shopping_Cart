ed83e6b87738f086a42e5652bed5f0ab4c4aa0b1
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-13 19:21:23 -0300

    Req 5

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // event.target.remove();
+  event.target.remove();
 };
 
 
