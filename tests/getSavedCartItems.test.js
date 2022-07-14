ca2c0043325e703ecfa52d96af2e72e40f457db5
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-14 18:02:33 -0300

    Req 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,14 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    getSavedCartItems();
+    expect.assertions(1);
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
+    getSavedCartItems();
+    expect.assertions(1);
+    expect(localStorage.getItem).toHaveBeenCalledWith("cartItems");
+  });
 });
