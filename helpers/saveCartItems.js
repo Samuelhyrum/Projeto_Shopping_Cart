c14ea8d217ffd7b51a64a8f3cbd2c85b35e218c0
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-14 17:45:54 -0300

    Requisito 6

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,7 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (parametro) => {
+  if (parametro === undefined) {
+    return new Error('So... nothing was added');
+  } localStorage.setItem('cartItems', parametro);
 };
 
 if (typeof module !== 'undefined') {
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,19 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect.assertions(1);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems ', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect.assertions(1);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
+  it('Sem parametro retorna um erro personalizado', () => {
+    const response = saveCartItems();
+    expect.assertions(1);
+    expect(response).toEqual(new Error('So... nothing was added'));
+  });
 });
